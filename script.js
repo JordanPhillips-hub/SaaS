d49eedeb284c9a0de367ec059dabff75ce8735cb
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-05-15 10:28:52 -0600

    moved nav dropdown, fixed dropdown not opening

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const navButton = document.querySelector('button[aria-expanded]');
+navButton.addEventListener('click', toggleNav);
 
     function toggleNav({ target }) {
       const expanded = target.getAttribute('aria-expanded') === 'true' || false;
       navButton.setAttribute('aria-expanded', !expanded);
     }
 
-    navButton.addEventListener('click', toggleNav);
 
     
